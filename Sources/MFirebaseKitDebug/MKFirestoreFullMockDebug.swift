913f58f8d5f4635227a5adc7342b8bf939c1ad14
joschuamarz <mail@joschuamarz.de>
2024-04-27 17:23:07 +0200

    Update imports

--- Sources/MFirebaseKitDebug/MKFirestoreFullMockDebug.swift
+++ Sources/MFirebaseKitDebug/MKFirestoreFullMockDebug.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import XCTest
-@testable import MFirebaseKit
+import MFirebaseKit
 
 extension Array {
     mutating func removeFirst(where shouldBeRemoved: (Element) throws -> Bool) rethrows -> Element? {
