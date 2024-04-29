71e20816912fcc46eeeb6c59929be66d0179e283
joschuamarz <mail@joschuamarz.de>
2024-04-29 18:34:37 +0200

    Update package deps

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         ),
         .target(
             name: "MFirebaseKitDebug",
-            dependencies: ["MFirebaseKit"]
+            dependencies: []
         ),
         .testTarget(
             name: "MFirebaseKitTests",
