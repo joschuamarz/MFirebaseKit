63cbac14e58b0da5a06358fed04a679f704f4683
joschuamarz <mail@joschuamarz.de>
2024-04-29 20:23:07 +0200

    Add tests

---
+++ Tests/MFirebaseKitTests/Tester.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joschua Marz on 29.04.24.
+//
+
+import Foundation
+
+public struct Tester {
+    public init() { }
+    
+    public func log(_ message: String) {
+        print(message)
+    }
+}
